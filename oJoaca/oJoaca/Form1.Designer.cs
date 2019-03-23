73f7c1e9a23b322eec4690f0a7c40d4143d0a210
Catalin Belcianu <catalin.belcianu@gmail.com>
2019-03-23 08:03:14 +0200

    Implemented lab1

---
+++ oJoaca/oJoaca/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace oJoaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridView1.Location = new System.Drawing.Point(41, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(637, 197);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView2.DefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGridView2.Location = new System.Drawing.Point(41, 246);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(637, 197);
+            this.dataGridView2.TabIndex = 1;
+            this.dataGridView2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(913, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(888, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(888, 150);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(888, 178);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(888, 206);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(888, 234);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(1011, 234);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(1011, 206);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 11;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(1011, 178);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 10;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(1011, 150);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 22);
+            this.textBox9.TabIndex = 9;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(1011, 122);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 22);
+            this.textBox10.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(913, 291);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1011, 262);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1400, 522);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ oJoaca/oJoaca/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oJoaca
+{
+    public partial class Form1 : Form
+    {
+        private String conStr = @"Data Source=DESKTOP-740KALD\SQLEXPRESS; Initial Catalog=ArtGallery; Integrated Security=true";
+
+        public Form1()
+        {
+            InitializeComponent();
+            myMethod();
+        }
+
+        public void myMethod()
+        {
+            using (SqlConnection dbCon = new SqlConnection(this.conStr))
+            {
+                dbCon.Open();
+                SqlDataAdapter dta = new SqlDataAdapter("SELECT * FROM Artists", dbCon);
+                DataTable tbl = new DataTable();
+                dta.Fill(tbl);
+                dataGridView1.DataSource = tbl;
+                SqlDataAdapter dta1 = new SqlDataAdapter("SELECT * FROM Arts", dbCon);
+                DataTable tbl1 = new DataTable();
+                dta1.Fill(tbl1);
+                dataGridView2.DataSource = tbl1;
+                //dataGridView1.Columns["ArtistID"].Visible = false;
+                dbCon.Close();
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int rowIndex = e.RowIndex;
+            int aid = Int32.Parse(dataGridView1[0, rowIndex].Value.ToString());
+            SqlConnection dbCon = new SqlConnection(this.conStr);
+            SqlCommand cmd = new SqlCommand("select * from Arts where Arts.ArtistID=@aid",dbCon);
+            SqlParameter pr = new SqlParameter("@aid", aid);
+            cmd.Parameters.Add(pr);
+            SqlDataAdapter dta = new SqlDataAdapter(cmd);
+            DataTable tbl = new DataTable();
+            dta.Fill(tbl);
+            dataGridView2.DataSource = tbl;
+            //dataGridView2.Columns["ArtistID"].Visible = false;
+            //dataGridView2.Columns["SectionID"].Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int artid = Int32.Parse(textBox1.Text);
+            int artistid = Int32.Parse(textBox2.Text);
+            int sectionid = Int32.Parse(textBox3.Text);
+            String title = textBox4.Text;
+            int price = Int32.Parse(textBox5.Text);
+            SqlConnection dbCon = new SqlConnection(this.conStr);
+            SqlCommand cmd = 
+                new SqlCommand("insert into Arts(ArtID, ArtistID, SectionID, ArtTitle, ArtPrice) values (@artid,@artistid,@sectionid,@title,@price)", dbCon);
+            SqlParameter paid = new SqlParameter("@artid", artid);
+            SqlParameter parid = new SqlParameter("@artistid", artistid);
+            SqlParameter psid = new SqlParameter("@sectionid", sectionid);
+            SqlParameter ptitle = new SqlParameter("@title", title);
+            SqlParameter pprice = new SqlParameter("@price", price);
+            cmd.Parameters.Add(paid); cmd.Parameters.Add(parid); cmd.Parameters.Add(psid); cmd.Parameters.Add(ptitle); cmd.Parameters.Add(pprice);
+            dbCon.Open();
+            cmd.ExecuteNonQuery();
+            dbCon.Close();
+            myMethod();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int artid = Int32.Parse(textBox1.Text);
+            int artprice = Int32.Parse(textBox2.Text);
+            String title = textBox3.Text;
+            int artistid = Int32.Parse(textBox4.Text);
+            int sectionid = Int32.Parse(textBox5.Text);
+
+            SqlConnection dbCon = new SqlConnection(this.conStr);
+            SqlCommand cmd =
+                new SqlCommand("delete from Arts where ArtID=@artid", dbCon);
+            SqlParameter paid = new SqlParameter("@artid", artid);
+            cmd.Parameters.Add(paid);
+            dbCon.Open();
+            cmd.ExecuteNonQuery();
+            dbCon.Close();
+            myMethod();
+        }
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int rowIndex = e.RowIndex;
+            int artid = Int32.Parse(dataGridView2[0, rowIndex].Value.ToString());
+            int artprice = Int32.Parse(dataGridView2[1, rowIndex].Value.ToString());
+            String title = dataGridView2[2, rowIndex].Value.ToString();
+            int artistid = Int32.Parse(dataGridView2[3, rowIndex].Value.ToString());
+            int sectionid = Int32.Parse(dataGridView2[4, rowIndex].Value.ToString());
+
+            textBox1.Text = artid.ToString();
+            textBox2.Text = artprice.ToString();
+            textBox3.Text = title;
+            textBox4.Text = artistid.ToString();
+            textBox5.Text = sectionid.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int artid = Int32.Parse(textBox1.Text);
+            int artprice = Int32.Parse(textBox2.Text);
+            String title = textBox3.Text;
+            int artistid = Int32.Parse(textBox4.Text);
+            int sectionid = Int32.Parse(textBox5.Text);
+
+            int uartid = Int32.Parse(textBox6.Text);
+            int uartprice = Int32.Parse(textBox7.Text);
+            String utitle = textBox8.Text;
+            int uartistid = Int32.Parse(textBox9.Text);
+            int usectionid = Int32.Parse(textBox10.Text);
+
+            SqlConnection dbCon = new SqlConnection(this.conStr);
+            SqlCommand cmd =
+                new SqlCommand("update Arts set ArtTitle=@atitle, ArtPrice=@aprice where ArtID=@artid", dbCon);
+            SqlParameter paid = new SqlParameter("@artid", artid);
+            SqlParameter ptitle = new SqlParameter("@atitle", utitle);
+            SqlParameter pprice = new SqlParameter("@aprice", uartprice);
+            cmd.Parameters.Add(paid); cmd.Parameters.Add(ptitle); cmd.Parameters.Add(pprice);
+            dbCon.Open();
+            cmd.ExecuteNonQuery();
+            dbCon.Close();
+            myMethod();
+
+        }
+    }
+}
